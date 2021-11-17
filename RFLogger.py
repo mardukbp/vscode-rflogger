ef74ec5db48f001081867fc2545d95ea94df9815
Marduk Bolaños <marduk.bolanos@imbus.de>
2021-11-17 15:51:35 +0100

    The log message has now the same format as in RED

--- RFLogger.py
+++ RFLogger.py
@@ -7,7 +7,7 @@ def start_suite(name, attributes):
     send(None, "clear")
 
 def log_message(message):   
-    send(f"{message['level']} {message['message']}".encode('utf-8'), "log")
+    send(f"{message['timestamp']} : {message['level']} : {message['message']}".encode('utf-8'), "log")
 
 def send(body, endpoint):
     requests.post(LISTENER_SERVER + endpoint, data = body)
