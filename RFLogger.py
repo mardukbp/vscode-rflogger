9aafe076ca95ae8014b72b7d6f8240b67b6770de
Marduk Bolaños <marduk.bolanos@imbus.de>
2021-12-20 14:39:42 +0100

    Indent nested keyword calls

--- RFLogger.py
+++ RFLogger.py
@@ -2,19 +2,39 @@ import requests
 
 ROBOT_LISTENER_API_VERSION = 2
 LISTENER_SERVER = "http://localhost:5696/"
+INDENT = 4*" "
+depth = 0
+
+def keyword_was_run(attributes):
+    type = attributes['type']
+    status = attributes['status']
+
+    return type == 'KEYWORD' and not status == 'NOT RUN'
+
 
 def start_suite(name, attributes):
     send('clear', None)
 
-def start_keyword(name, attributes):
-    type = attributes['type']
-    status = attributes['status']
 
-    if type == 'KEYWORD' and not status == 'NOT RUN':
+def start_keyword(name, attributes):
+    global depth
+
+    if keyword_was_run(attributes):
         send('log', f"Keyword: {name}")
+        depth += 1
+
+
+def end_keyword(name, attributes):
+    global depth
+
+    if keyword_was_run(attributes):
+        depth -= 1
+
 
 def log_message(message):   
     send('log', f"{message['level']} : {message['message']}")
 
+
 def send(endpoint, body):
-    requests.post(LISTENER_SERVER + endpoint, data = body.encode('utf-8') if body else body)
+    data = (depth*INDENT + body).encode('utf-8') if body else body
+    requests.post(LISTENER_SERVER + endpoint, data)
